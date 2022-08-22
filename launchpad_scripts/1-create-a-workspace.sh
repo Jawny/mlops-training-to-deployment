f1d4e5e8aa21dcd8fc30d5f8b4101692971c5834
johnny <johnnyliao06@gmail.com>
2022-08-22 14:20:24 -0700

    updated create a workspace script to use correct github link and removed job id issue

--- launchpad_scripts/1-create-a-workspace.sh
+++ launchpad_scripts/1-create-a-workspace.sh
@@ -7,5 +7,4 @@ training_git_url="https://github.com/Jawny/mlops-training-to-deployment.git"
 ngc workspace create --name "${training_workspace_name}" | tee training_workspace_output.log
 # Grab the workspace id from the log file and create
 # a job that runs git clone on the git_url
-training_job_id=`grep 'Successfully created workspace with ID:' workspace_output.log | awk '{print $5}' | tr -d "'"`
-ngc batch run --name "${training_workspace_name}" --preempt RUNONCE --min-timeslice 0s --total-runtime 0s --instance cpu.x86.tiny --commandline "cd /mnt/workspace; git clone ${training_git_url}" --result /results --image "nvidian/pytorch:22.06-py3" --workspace $training_job_id:/mnt/workspace:RW
+ngc batch run --name "${training_workspace_name}-setup" --preempt RUNONCE --min-timeslice 0s --total-runtime 0s --instance cpu.x86.tiny --commandline "cd /mnt/workspace; git clone ${training_git_url}" --result /results --image "nvidian/pytorch:22.06-py3" --workspace $training_workspace_name:/mnt/workspace:RW
